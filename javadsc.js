dd7e2deb948701757ba3f341a58a2640b35f581c
mayamatei <98939634+mayamatei@users.noreply.github.com>
2023-05-30 04:58:50 +0200

    Add files via upload

---
+++ javadsc.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('myVideo');
+    video.controls = false; // Hide the default controls
+    video.play(); // Start playing the video automatically
+  });
+
+
+
+var images = new Array();
+
+
+
